0acf8d98a347f6bc2f56d8b12ecc5703e0d18393
Sourav Halder <souravhalder@convertcart.com>
2019-03-06 17:55:14 +0530

    Updated index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const got = require('got');
 const existingHosts = [];
 const traefikHost = process.env.TRAEFIK_HOST_NAME;
+const cfEmail = process.env.CLOUDFLARE_EMAIL;
+const cfToken = process.env.CLOUDFLARE_TOKEN;
 function checkConfig() {
   console.log('checking');
   (async () => {
@@ -8,7 +10,7 @@ function checkConfig() {
         const response = await got('http://localhost:8080/api/providers/kubernetes/frontends');
         // console.log('sourav-traefik-docker: ', response.body);
         const hosts = Object.keys(response.body).map(str => str.replace(/\/$/, ''));
-        const hostRegExp = new RegExp(traefikHost);
+        const hostRegExp = new RegExp(`${traefikHost}`);
         const respMeta = await got('http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip', {
           headers: {
             "Metadata-Flavor": "Google"
@@ -19,6 +21,17 @@ function checkConfig() {
         console.log('traefik host: ', traefikHost);
         console.log('machine ip: ', traefikHostIp);
         console.log('allowed hosts: ', allowedHosts);
+
+        const cloudResp = await got(`https://api.cloudflare.com/client/v4/zones?name=${traefikHost}`, {
+          headers: {
+            "X-Auth-Email": cfEmail,
+            "X-Auth-Key": cfToken,
+            "Content-Type": "application/json"
+          }
+        });
+        const zoneId = cloudResp.result[0].id;
+        console.log('zoneId: ', zoneId);
+
       } catch (error) {
         console.error('sourav-traefik-docker: ', error);
       }
